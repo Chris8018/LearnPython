b17989688a776c79299221bb472293f6577e9e10
Chris8018 <tranuytrieuvi@gmail.com>
2019-02-11 15:50:40 +0000

    Add Tutorial 2

---
+++ src/Python/2/2.py
@@ -0,0 +1,27 @@
+# Password generator
+# **Online Solution**
+
+import string
+import random
+
+
+def gen_random_string(size=5, chars=string.ascii_letters + string.digits):
+    """
+    @:arg1 size = 8
+    @:arg2 chars = letters + digits
+    @:return a string
+    """
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def histogram_from_list(the_list):
+    temp_set = set(the_list)
+    for x in temp_set:
+        print('{0} {1}'.format(x, '*'*the_list.count(x)))
+    pass
+
+
+list_string = [gen_random_string() for _ in range(5)]
+
+my_list = [list_string[random.randint(0, len(list_string) - 1)] for _ in range(10)]
+histogram_from_list(my_list)
