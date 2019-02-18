f7f0923524f0c2b434ebb4b3645a488ac5998759
Chris8018 <tranuytrieuvi@gmail.com>
2019-02-18 05:53:50 +0000

    Add e19

--- src/Python/2/e17.py
+++ src/Python/2/e17.py
@@ -1,3 +1,8 @@
+"""
+Include e19
+write the site to a file
+"""
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,8 +13,18 @@ urls = ['https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humili
 url = urls[0]
 
 # Request URL
-soup = BeautifulSoup(requests.get(url).text, "lxml")
+# soup = BeautifulSoup(requests.get(url).text, "lxml")
+#
+#
+# for line in soup.find_all('p'):
+#     print(line.getText())
 
 
-for line in soup.find_all('p'):
-    print(line.getText())
+def write_site_article_to_file(url):
+    soup = BeautifulSoup(requests.get(url).text, "lxml")
+    with open('e19_site_content.txt', 'w') as file:
+        for line in soup.find_all('p'):
+            file.write(line.getText() + '\n')
+
+
+write_site_article_to_file(url)
